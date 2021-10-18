eb281d4ecc17490c8849a6288f9fce9788078d5c
Luis Rivera <luis.rivera@infotec.mx>
2021-10-18 17:27:20 -0500

    Products and Nav components

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImgComponent implements OnInit {
 
+  img: string = '';
+
+  @Input('img')
+  set changeImg(newImg: string) {
+    this.img = newImg;
+    console.log('change just img  =>' ,this.img);
+    // code
+  }
+
+  imageDefault = './assets/images/default.png';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  imgError() {
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded() {
+    console.log('Image loaded correctly');
+    //this.loaded.emit(this.img);
   }
 
 }
